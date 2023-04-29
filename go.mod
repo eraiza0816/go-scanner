1bdfa4012a9602f4fe7369783a5c3ec4d3d6e317
eraiza0816 <programeraiza@hotmail.co.jp>
2023-04-30 01:30:19 +0900

    use s3

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/eraiza0816/go-scanner
 
 go 1.20
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -5,7 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
+
+var filePath = "./output.csv"
+var bucket = "code_scan"
+var key = "code/output.csv"
+var awsRegion = "ap-northeast-1"
 
 func main() {
 	file, err := os.OpenFile("output.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
@@ -19,4 +27,31 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 	writer.Write([]string{stdin})
+
+	// sessionを作成します
+	newSession := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// S3クライアントを作成します
+	svc := s3.New(newSession, &aws.Config{
+		Region: aws.String(awsRegion),
+	})
+
+	// S3にアップロードする内容をparamsに入れます
+	params := &s3.PutObjectInput{
+		// Bucket アップロード先のS3のバケット
+		Bucket: aws.String(bucket),
+		// Key アップロードする際のオブジェクト名
+		Key: aws.String(key),
+		// Body アップロードする画像ファイル
+		Body: "output.csv",
+
+	// S3にアップロードします
+	_, err == svc.PutObject(params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("uploaded to S3")
+	}
 }
